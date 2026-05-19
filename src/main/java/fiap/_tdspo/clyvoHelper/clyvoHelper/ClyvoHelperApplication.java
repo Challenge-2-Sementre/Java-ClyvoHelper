0c81806055fac68db4820a80d65e75e654a411ad
viniciussoteras@gmail.com <labsfiap@fiap.com.br>
2026-05-18 21:59:40 -0300

    initial commit

---
+++ src/main/java/fiap/_tdspo/clyvoHelper/clyvoHelper/ClyvoHelperApplication.java
@@ -0,0 +1,13 @@
+package fiap._tdspo.clyvoHelper.clyvoHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvoHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClyvoHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/_tdspo/clyvoHelper/clyvoHelper/ClyvoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap._tdspo.clyvoHelper.clyvoHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClyvoHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
