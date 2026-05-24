7558c479e14cc9802b12519240728b8b86f24519
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-05-24 00:56:21 -0300

    agendamentos funcionando

--- src/main/java/fiap/_tdspo/clyvoHelper/clyvoHelper/controller/AgendamentoController.java
+++ src/main/java/fiap/_tdspo/clyvoHelper/clyvoHelper/controller/AgendamentoController.java
@@ -8,7 +8,6 @@ import fiap._tdspo.clyvoHelper.clyvoHelper.repository.AgendamentoRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @RestController
